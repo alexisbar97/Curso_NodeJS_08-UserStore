5e56f7f1c7f44153ea5b5158e2dbe0aeac76b9f9
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2025-04-07 21:42:47 -0400

    12. Video 273 Finalizado

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,10 +1,12 @@
 import jwt from 'jsonwebtoken';
-import { parse } from 'path';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
     static async generateToken(payload: any, duration: string = '2h') {
         return new Promise((resolve) => {
-            jwt.sign(payload, "SEED", { expiresIn: parseInt(duration) }, (err, token) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: parseInt(duration) }, (err, token) => {
                 if (err) {
                     resolve(err);
                 }
